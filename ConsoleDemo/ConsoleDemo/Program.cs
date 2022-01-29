cc70dff13f80dcb4db5bde34aa2c34a53c92c037
mcclain113 <barrymcclainrx@gmail.com>
2022-01-29 15:23:32 -0600

    first commit

---
+++ ConsoleDemo/ConsoleDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ConsoleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //read file
+            string file = "Files/grades.txt";
+            if (File.Exists(file))
+            {
+                StreamReader sr = new StreamReader(file);
+                string line = sr.ReadLine();
+                line.Split(',');
+
+                while (!sr.EndOfStream) //test
+                {
+                    line = sr.ReadLine();
+                    var column = line.Split(',');
+
+                    Console.WriteLine("Name {0}, Class {1}, Grade {2}", column[0], column[1], column[2]);
+                }
+
+
+
+                sr.Close();
+                //write file
+                StreamWriter sw = new StreamWriter(file, true);
+
+                sw.Close();
+            }
+
+            
+        }
+    }
+}
